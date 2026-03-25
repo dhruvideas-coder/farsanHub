73bdfdd8db7edc8d9bbeddbdbdf81779469519d6
Dhruv Patel <dhruv@selcom.net>
2026-03-26 00:28:49 +0530

    basic change for check git auto depoloy is working or not

--- resources/views/admin/parts/sidebar.blade.php
+++ resources/views/admin/parts/sidebar.blade.php
@@ -85,7 +85,7 @@
                     <small class="sidebar-brand-sub d-block">{{ auth()->user()->name ?? '' }}</small>
                 </div>
             </div>
-            <button class="sidebar-close-btn" data-bs-dismiss="offcanvas" type="button" aria-label="Close">
+            <button class="sidebar-close-btn mt-2" data-bs-dismiss="offcanvas" type="button" aria-label="Close">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
                     fill="none" stroke="currentColor" stroke-width="2.5"
                     stroke-linecap="round" stroke-linejoin="round">
