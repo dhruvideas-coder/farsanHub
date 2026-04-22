b945a1eb9bc4c72056c89f3cebfed0d7d9f6a2ec
Dhruv Patel <dhruv@selcom.net>
2026-04-22 22:58:15 +0530

    change default url as login page url on hit domain

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,8 @@ use Illuminate\Support\Facades\Route;
 // Route::post('/register', [App\Http\Controllers\AuthController::class, 'register'])->name('register.post');
 Route::get('/admin', [App\Http\Controllers\AuthController::class, 'showLogin'])->name('login');
 Route::get('/', function () {
-    return view('webpage');
+    // return view('webpage');
+    return redirect()->route('login');
 })->name('home');
 Route::post('/login', [App\Http\Controllers\AuthController::class, 'login'])->name('login.post');
 Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->name('logout');
